c89f80325597c60db71f4a59d3368ad3bb18bb1a
unknown <alauddinnajim202@gmail.com>
2025-06-23 14:39:24 +0600

    refactor redirect logic in CustomRedirectMiddleware

--- app/Http/Middleware/CustomRedirectMiddleware.php
+++ app/Http/Middleware/CustomRedirectMiddleware.php
@@ -11,14 +11,9 @@ class CustomRedirectMiddleware
     public function handle(Request $request, Closure $next)
     {
         if (Auth::guard('web')->check() && Auth::guard('web')->user()->status == 'active') {
-            if (Auth::guard('web')->user()->hasRole('developer')) {
-                return redirect()->intended(route('developer.dashboard', absolute: false));
-            }elseif (Auth::guard('web')->user()->hasRole('admin')) {
+
                 return redirect()->intended(route('admin.dashboard', absolute: false));
-            }else{
-                Auth::logout();
-                return redirect()->intended(route('login', absolute: false));
-            }
+
         }
 
         return redirect()->intended(route('home', absolute: false));
